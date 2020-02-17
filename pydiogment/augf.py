615d451e79963e2c8683fc6582eb3aa80ec350c2
SuperKogito <kogito@hotmail.fr>
2020-02-17 16:27:24 +0100

    add filters call code

--- pydiogment/augf.py
+++ pydiogment/augf.py
@@ -1,6 +1,5 @@
 """
 - Description: frequency based augmentation techniques/manipulations for audio data.
-- Author: Ayoub Malek
 """
 import os
 import math
@@ -12,21 +11,21 @@ import numpy as np
 from .io import read_file, write_file
 
 
-def convolve(infile, ir_name, level=0.5):
+def convolve(infile, ir_fname, level=0.5):
     """
     apply convolution to infile using the given impulse response file.
 
     Args:
-        infile  (str) : input filename/path.
-        ir_name (str) : name of impulse response file.
-        level (float) : can be between 0 and 1, default value = 0.5
+        infile   (str) : input filename/path.
+        ir_fname (str) : name of impulse response file.
+        level  (float) : can be between 0 and 1, default value = 0.5
     """
     # read input file
     fs1, x = read_file(filename=infile)
     x = np.copy(x)
 
     # change the path below for the sounds folder
-    ir_path = 'pydiogment/sounds/{0}.wav'.format(ir_name)
+    ir_path = 'pydiogment/sounds/{0}.wav'.format(ir_fname)
     fs2, ir = read_file(filename=ir_path)
 
     # apply convolution
@@ -38,7 +37,7 @@ def convolve(infile, ir_name, level=0.5):
     # export data to file
     input_file_name = os.path.basename(infile)
     output_file_path = os.path.dirname(infile)
-    name_attribute = "_augmented_{0}_convolved_with_level_{1}.wav".format(ir_name, level)
+    name_attribute = "_augmented_{0}_convolved_with_level_{1}.wav".format(ir_fname, level)
     write_file(output_file_path=output_file_path,
                input_file_name=infile,
                name_attribute=name_attribute,
@@ -71,3 +70,34 @@ def change_tone(infile, tone):
                          stdin=subprocess.PIPE,
                          stdout=subprocess.PIPE,
                          stderr=subprocess.PIPE)
+
+
+def apply_filter(infile, filter_type, low_cutoff_freq, high_cutoff_freq=None, order=5):
+    """
+    apply a certain type of Buttenworth filter on the input audio.
+
+    Args:
+        infile             (str) : input audio filename.
+        filter_type        (str) : type of the filter to apply.
+        low_cutoff_freq  (float) : the low cut-off frequency of the filter.
+        high_cutoff_freq (float) : the high cut-off frequency of the filter.
+        order              (int) : filter order to define its accuracy.
+    """
+    # read input file
+    fs, sig = read_file(filename=infile)
+
+    # apply filter
+    y = butter_filter(sig=sig, fs=fs, ftype=filter_type,
+                      low_cut=low_cutoff_freq,
+                      high_cut=high_cutoff_freq,
+                      order=order)
+
+    # export data to file
+    input_file_name = os.path.basename(infile)
+    output_file_path = os.path.dirname(infile)
+    name_attribute = "_augmented_{0}_pass_filtered.wav".format(filter_type)
+    write_file(output_file_path=output_file_path,
+               input_file_name=infile,
+               name_attribute=name_attribute,
+               sig=y,
+               fs=fs)
