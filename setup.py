3ed3977774ceb89ea03f5fb1e8740278a6e2f86f
Ayoub Malek <kogito@hotmail.fr>
2020-02-22 13:37:58 +0100

    add setup files

---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+path = pathlib.Path(__file__).parent
+
+# get readme text
+readme = (path / "README.md").read_text()
+# define requirements
+requires = ["numpy>=1.17.2", "scipy>=1.3.1"]
+
+setup (
+        name         = 'pydiogment',
+        version      = '0.1.0',
+        author       = 'SuperKogito',
+        author_email = 'superkogito@gmail.com',
+        description  = 'Python audio augmentation',
+        long_description = readme,
+        long_description_content_type = "text/markdown",
+        license      = 'BSD',
+        url          = 'https://github.com/SuperKogito/pydiogment',
+        packages     = find_packages(),
+        classifiers  = [
+                        'Development Status :: 3 - Alpha',
+                        'Environment :: Console',
+                        'Environment :: Web Environment',
+                        'Intended Audience :: Developers',
+                        'License :: OSI Approved :: BSD License',
+                        'Operating System :: OS Independent',
+                        'Programming Language :: Python',
+                        'Topic :: Documentation',
+                        'Topic :: Utilities',
+                      ],
+        platforms            = 'any',
+        include_package_data = True,
+        install_requires     = requires,
+     )
